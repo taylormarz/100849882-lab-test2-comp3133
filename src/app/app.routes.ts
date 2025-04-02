01a8a419de046ada4976e72339b9defb46994262
Taylor Martin <taylormartind@gmail.com>
2025-04-02 18:07:57 -0400

    added mission details path to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { MissionListComponent } from './mission-list/mission-list.component';
+import { MissionDetailsComponent } from './mission-details/mission-details.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: '/missions', pathMatch: 'full' },
 	{ path: 'missions', component: MissionListComponent },
+	{ path: 'missions/:id', component: MissionDetailsComponent }
+
 ];
