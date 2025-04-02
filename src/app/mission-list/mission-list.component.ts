bbd245309259be26de8074de4db72800fe902285
Taylor Martin <taylormartind@gmail.com>
2025-04-02 10:44:13 -0400

    created missionlist component

---
+++ src/app/mission-list/mission-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-list',
+  imports: [],
+  templateUrl: './mission-list.component.html',
+  styleUrl: './mission-list.component.css'
+})
+export class MissionListComponent {
+
+}
