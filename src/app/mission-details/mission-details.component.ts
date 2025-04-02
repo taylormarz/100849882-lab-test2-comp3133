00fc0c9abb095d97368fbdad1193c4580446300a
Taylor Martin <taylormartind@gmail.com>
2025-04-02 18:03:07 -0400

    created mission details component

---
+++ src/app/mission-details/mission-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-details',
+  imports: [],
+  templateUrl: './mission-details.component.html',
+  styleUrl: './mission-details.component.css'
+})
+export class MissionDetailsComponent {
+
+}
