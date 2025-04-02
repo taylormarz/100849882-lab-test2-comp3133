ffa1c4fa1a11963f990cfffd2230e8f2e370155a
Taylor Martin <taylormartind@gmail.com>
2025-04-02 19:03:03 -0400

    final commit before deployment

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: '100849882-lab-test2-comp3133',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: '100849882-lab-test2-comp3133',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
